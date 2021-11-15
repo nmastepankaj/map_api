8f1b63c47c4063b5a345dec4401db3a1b1e40d19
Pankaj Singh Rana <pankajsinghrana2003@gmail.com>
2021-11-15 14:07:26 +0530

    Initial Commit

---
+++ js/corona-data.js
@@ -0,0 +1,49 @@
+var lastrange = '70000 to 75000';
+var lastrangetext = 'Above 70000';
+var jsonColorString = [
+        "#66022b@70000 to 75000",
+        "#d53e4f@50001 to 70000",
+        "#f46d43@5001 to 50000",
+        "#fabf41@1001 to 5000",
+        "#f0f598@10 to 1000",
+        "#9de69b@0 to 0"
+    ];
+    
+var jsonStateString = {
+	26:"Andhra Pradesh#3233#2051082#14403",
+	33:"Andaman and Nicobar Islands#9#7527#129",
+	12:"Arunachal Pradesh#44#54873#280",
+	18:"Assam#3301#603463#6034",
+	10:"Bihar#34#716443#9661",
+	4:"Chandigarh#26#64525#820",
+	22:"Chhattisgarh#233#992401#13586",
+	7:"Delhi#349#1414736#25091",
+	28:"Goa#285#174712#3370",
+	24:"Gujarat#209#816485#10090",
+	6:"Haryana#116#761189#10050",
+	2:"Himachal Pradesh#1083#220297#3785",
+	1:"Jammu and Kashmir#1199#327681#4445",
+	20:"Jharkhand#154#343641#5138",
+	27:"Karnataka#7984#2944422#38122",
+	30:"Kerala#71644#4921312#34362",
+	35:"Ladakh#125#20738#209",
+	23:"Madhya Pradesh#88#782307#10524",
+	25:"Maharashtra#16943#6461956#140430",
+	14:"Manipur#707#121541#1938",
+	15:"Mizoram#5782#120151#453",
+	21:"Odisha#2650#1033027#8364",
+	32:"Puduchery#267#126134#1862",
+	3:"Punjab#229#585821#16566",
+	8:"Rajasthan#45#945470#8954",
+	31:"Tamil Nadu#10271#2664247#36238",
+	34:"Telengana#3754#665101#3968",
+	16:"Tripura#147#83661#816",
+	5:"Uttarakhand#150#336414#7402",
+	9:"Uttar Pradesh#85#1687234#22903",
+	19:"West Bengal#7916#1572711#19252",   
+	36:"Dadra & Nagar Haveli And Daman & Diu#0#10678#4",
+	11:"Sikkim#122#31536#400",
+	13:"Nagaland#180#31082#689",
+	17:"Meghalaya#319#82135#1460",
+	29:"Lakshadweep#0#10314#51"
+};
